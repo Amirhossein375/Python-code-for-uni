30c8c8e46d0c6350d7bb16e6ed74cbbec6e02b52
Amirhossein <105815436+Amirhossein375@users.noreply.github.com>
2024-11-16 14:49:01 +0330

    Update Q3.py

--- Q3.py
+++ Q3.py
@@ -2,9 +2,9 @@ side1 = float(input("Inter side1: "))
 side2 = float(input("Inter side1: "))
 side3 = float(input("Inter side1: "))
 if side1 + side2 > side3 and side1 + side3 >side2 and side2 + side3 > side1 :
-  if side1 = side2 = side3 :
+  if side1 == side2 == side3 :
   print("This is an equilateral triangle")
-  elif side1 = side2 or side1 = side3 or side2 = side3 :
+  elif side1 == side2 or side1 == side3 or side2 == side3 :
   print("This is an isosceles triangle")
   else print("this is common triangle")
 else : 
