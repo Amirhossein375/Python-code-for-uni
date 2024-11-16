05c5f986325035483dd4f780c99e84dbeb77fac9
Amirhossein <105815436+Amirhossein375@users.noreply.github.com>
2024-11-16 14:16:19 +0330

    Update Q2.py

--- Q2.py
+++ Q2.py
@@ -1,5 +1,5 @@
 amount = float(input("Inter amount of rice (Kg):"))
-price = float(input(" Inter price of rice :")
+price = float(input(" Inter price of rice :"))
 min = amount* price - amount*price*0.15
 max = amount* price + amount*price*0.15
 print(f" min of price is {min} and max of price is {max}")
