a5e217d78786f2fbf39138290501fa39f99e9ec0
Amirhossein <105815436+Amirhossein375@users.noreply.github.com>
2024-11-19 20:06:40 +0330

    Update Q6.py

--- Q6.py
+++ Q6.py
@@ -1,1 +1,9 @@
+watertem = int(input("Please Inter temperature of water :"))
+if watertem > 100 :
+    print("steam")
+elif watertem >= 0 and watertem<= 100:
+    print("water")
+else : 
+    print("Ice")
+
 
