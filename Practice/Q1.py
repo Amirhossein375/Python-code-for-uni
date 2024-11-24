77d6dbae0221e4020f8583093b249dd0a311b2b3
Amirhossein <105815436+Amirhossein375@users.noreply.github.com>
2024-11-24 23:54:43 +0330

    Update Q1.py

--- Practice/Q1.py
+++ Practice/Q1.py
@@ -1,8 +1,8 @@
 number1 = int(input("please Inter number :"))
 number2 = int(input("Please Inter number :"))
 if number1 < number2 :
-    for i in range(number1 , number2) :
+    for i in range(number1 + 1, number2) :
         print(i)
 else :
-    for i in range(number2 , number1 ) :
+    for i in range(number2 + 1, number1 ) :
         print(i)
